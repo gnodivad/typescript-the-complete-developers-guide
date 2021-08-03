5829d8a0feebc04f317fc6a22f5905f45bd24698
David Ong <dodwei.dev@gmail.com>
2021-08-03 21:05:05 +0800

    [features]Why Typed Arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -2,3 +2,10 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
