be5d7681ab3c389f2d153a3fac2fc03e166deec9
David Ong <dodwei.dev@gmail.com>
2021-08-03 21:08:05 +0800

    [features]Multiple Types in Arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -9,3 +9,14 @@ const myCar = carMakers.pop();
 
 // Prevent incompatible values
 carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
+// importantDates.push(100);
