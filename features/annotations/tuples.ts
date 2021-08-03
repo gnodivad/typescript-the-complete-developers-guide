45a66cb37012062d1df5d9ffdc77944b5819546b
David Ong <dodwei.dev@gmail.com>
2021-08-03 21:18:03 +0800

    [features]Why Tuples?

--- features/annotations/tuples.ts
+++ features/annotations/tuples.ts
@@ -10,3 +10,9 @@ const pepsi: Drink = ['brown', true, 40];
 // pepsi[2] = 'brown';
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
