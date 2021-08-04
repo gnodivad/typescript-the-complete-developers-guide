69e1c3b6807fc79334aef94b5f0f3e526e540d36
David Ong <dodwei.dev@gmail.com>
2021-08-04 21:27:59 +0800

    [features]Instance Method Modifiers

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,16 +1,17 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
@@ -19,5 +20,4 @@ class Car extends Vehicle {
 // vehicle.honk();
 
 const car = new Car();
-car.drive();
-car.honk();
+car.startDrivingProcess();
