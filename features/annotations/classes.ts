877acb8373f94f0b4cdccac0378253283d103731
David Ong <dodwei.dev@gmail.com>
2021-08-04 21:35:48 +0800

    [features]Fields with Inheritance

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -9,20 +9,20 @@ class Vehicle {
 const vehicle = new Vehicle('orange');
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log('vroom');
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
 
-//   startDrivingProcess(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+  private drive(): void {
+    console.log('vroom');
+  }
 
-// const vehicle = new Vehicle();
-// vehicle.drive();
-// vehicle.honk();
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
 
-// const car = new Car();
-// car.startDrivingProcess();
+const car = new Car('red');
+car.startDrivingProcess();
