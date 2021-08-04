7fbe2845f4441b566506253eab851f0dce82c74d
David Ong <dodwei.dev@gmail.com>
2021-08-04 20:48:28 +0800

    [features]Fixing Long Annotations with Interfaces

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,14 +1,16 @@
-const oldCivic = {
-  name: 'civic',
-  year: 2000,
-  broken: true,
-};
-
-const printVehicle = (vehicle: {
+interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
-}): void => {
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2002,
+  broken: true,
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Name: ${vehicle.year}`);
   console.log(`Name: ${vehicle.broken}`);
