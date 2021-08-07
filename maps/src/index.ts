04ceff582f09dec001aeb1dbd0507835c336e0d1
David Ong <dodwei.dev@gmail.com>
2021-08-07 20:44:45 +0800

    [maps]Add marker for Company

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,8 +2,11 @@
 
 import { CustomMap } from './CustomMap';
 import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap('map');
 
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
