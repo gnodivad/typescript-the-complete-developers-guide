74f741bcb4acdbbbe17f8bce2db0b08c44c6d064
David Ong <dodwei.dev@gmail.com>
2021-08-07 21:34:33 +0800

    [maps]Show marker's InfoWindow

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -6,6 +6,7 @@ interface Mappable {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -31,10 +32,10 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi there!',
+        content: mappable.markerContent(),
       });
 
-      infoWindow.open(this.googleMapm, marker);
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
