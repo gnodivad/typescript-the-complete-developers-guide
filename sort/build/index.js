46b9bed90aad6c4d4ec28b66d2198d9723e20f29
David Ong <dodwei.dev@gmail.com>
2021-08-08 08:33:55 +0800

    [sort]Type Guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -6,12 +6,16 @@ var Sorter = /** @class */ (function () {
     Sorter.prototype.sort = function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+            if (this.collection instanceof Array) {
+                for (var j = 0; j < length - i - 1; j++) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
+            }
+            if (typeof this.collection === 'string') {
             }
         }
     };
