a0266537f93e7f269ffa204a1b22b014dab0a08c
David Ong <dodwei.dev@gmail.com>
2021-08-12 15:24:38 +0800

    [stats]Generating HTML Reports

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,10 +3,11 @@ import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
 import { WinsAnalysis } from './analyzers/WindsAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Chelsea'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Chelsea'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
