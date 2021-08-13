31c8e5cadc8fc7cfb80ffff2b90cca70c6a4df05
David Ong <dodwei.dev@gmail.com>
2021-08-13 15:15:10 +0800

    [web]Dynamic Array Creation

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,7 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
+user.on('change', () => {});
+user.on('change', () => {});
 
-user.set({ age: 999 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+console.log(user);
