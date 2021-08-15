6909a73a7e1b8b973e373f026bb41e3ed4c3108d
David Ong <dodwei.dev@gmail.com>
2021-08-15 22:07:39 +0800

    [web]CollectionView Implementation

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,34 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 import { UserEdit } from './views/UserEdit';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'Name', age: 20 });
+// const user = User.buildUser({ name: 'Name', age: 20 });
 
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
 
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root element not found');
+// }
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
