60252c0060549823a9aec41ba7e91b2449039512
David Ong <dodwei.dev@gmail.com>
2021-08-16 10:59:40 +0800

    [server]Wiring Up Sessions

--- server/build/index.js
+++ server/build/index.js
@@ -3,10 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var cookie_session_1 = __importDefault(require("cookie-session"));
 var express_1 = __importDefault(require("express"));
 var loginRoutes_1 = require("./loginRoutes");
 var app = express_1.default();
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use(cookie_session_1.default({ keys: ['server'] }));
 app.use(loginRoutes_1.router);
 app.listen(3000, function () {
     console.log('Listening on port 3000');
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import cookieSession from 'cookie-session';
 import express, { Request, Response } from 'express';
 import { router } from './loginRoutes';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['server'] }));
 app.use(router);
 
 app.listen(3000, () => {
