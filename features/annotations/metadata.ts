f736c143cfe75e4b590bdc3dc717416c853cc554
David Ong <dodwei.dev@gmail.com>
2021-08-17 10:20:25 +0800

    [features]Practical Metadata

--- features/annotations/metadata.ts
+++ features/annotations/metadata.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+// const plane = {
+//   color: 'red',
+// };
 
 // Reflect.defineMetadata('note', 'hi there', plane);
 // Reflect.defineMetadata('height', 10, plane);
@@ -11,7 +11,30 @@ const plane = {
 // const height = Reflect.getMetadata('height', plane);
 // console.log(note);
 // console.log(height);
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
 
-const note = Reflect.getMetadata('note', plane, 'color');
-console.log(note);
+// const note = Reflect.getMetadata('note', plane, 'color');
+// console.log(note);
+
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('HI THERE')
+  fly(): void {
+    console.log('vrrrrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
