2334b2e1e28d8edf7b29b649a59a6d40f218b8d2
David Ong <dodwei.dev@gmail.com>
2021-08-17 10:51:09 +0800

    [server]A Few Fixups

--- server/build/index.js
+++ server/build/index.js
@@ -5,14 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var cookie_session_1 = __importDefault(require("cookie-session"));
 var express_1 = __importDefault(require("express"));
-var loginRoutes_1 = require("./loginRoutes");
-var controller_1 = require("./controllers/decorators/controller");
+var loginRoutes_1 = require("./routes/loginRoutes");
 require("./controllers/LoginController");
+var AppRouter_1 = require("./AppRouter");
 var app = express_1.default();
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(cookie_session_1.default({ keys: ['server'] }));
 app.use(loginRoutes_1.router);
-app.use(controller_1.router);
+app.use(AppRouter_1.AppRouter.getInstance());
 app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
 import cookieSession from 'cookie-session';
 import express, { Request, Response } from 'express';
-import { router } from './loginRoutes';
-import { router as controllerRoute } from './controllers/decorators/controller';
+import { router } from './routes/loginRoutes';
 import './controllers/LoginController';
+import { AppRouter } from './AppRouter';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['server'] }));
 app.use(router);
-app.use(controllerRoute);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
